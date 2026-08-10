f21e6f7554d11ab628aa90121c0b2a3a5cf1352f
claudezihsuen <claude.zihsuen@gmail.com>
2026-08-10 08:47:07 +0800

    Add files via upload

--- src/layouts/AppShell.tsx
+++ src/layouts/AppShell.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "@/components/Sidebar";
 import { BottomNav } from "@/components/BottomNav";
+import { TopBar } from "@/components/TopBar";
 import type { NavItem } from "@/types";
 
 interface AppShellProps {
@@ -12,11 +13,14 @@ export function AppShell({ items, roleLabel }: AppShellProps) {
   return (
     <div className="flex min-h-screen bg-canvas">
       <Sidebar items={items} roleLabel={roleLabel} />
-      <main className="flex-1 px-4 pb-24 pt-6 sm:px-6 lg:px-10 lg:pb-10 lg:pt-8">
-        <div className="mx-auto w-full max-w-5xl">
-          <Outlet />
-        </div>
-      </main>
+      <div className="flex min-w-0 flex-1 flex-col">
+        <TopBar />
+        <main className="flex-1 px-4 pb-24 pt-6 sm:px-6 lg:px-10 lg:pb-10 lg:pt-8">
+          <div className="mx-auto w-full max-w-5xl">
+            <Outlet />
+          </div>
+        </main>
+      </div>
       <BottomNav items={items} />
     </div>
   );
